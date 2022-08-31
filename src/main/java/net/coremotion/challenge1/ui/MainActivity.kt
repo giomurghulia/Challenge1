fd2a2719c1ce28ff8fb873a25131d22481288bdc
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2022-08-31 13:18:35 +0400

    finish

---
+++ src/main/java/net/coremotion/challenge1/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package net.coremotion.challenge1.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import net.commotion.challenge1.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
